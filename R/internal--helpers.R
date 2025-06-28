aa874ccec9ac7b8863e5c81e73875fc8fae9123a
Fabian Distler <fabian.distler@gmx.net>
2025-06-28 12:33:56 +0200

    prep for pr

--- R/internal--helpers.R
+++ R/internal--helpers.R
@@ -3,11 +3,17 @@ get_os_type <- function() {
   sys_info <- Sys.info()
   if (!is.null(sys_info)) {
     os <- sys_info["sysname"]
-    if (os == "Darwin") {os <- "mac"}
+    if (os == "Darwin") {
+      os <- "mac"
+    }
   } else {
     os <- .Platform$OS.type
-    if (grepl("^darwin",   R.version$os)) {os <- "mac"}
-    if (grepl("linux-gnu", R.version$os)) {os <- "linux"}
+    if (grepl("^darwin", R.version$os)) {
+      os <- "mac"
+    }
+    if (grepl("linux-gnu", R.version$os)) {
+      os <- "linux"
+    }
   }
   unname(tolower(os))
 }
